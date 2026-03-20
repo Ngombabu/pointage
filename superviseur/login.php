cdc471e5d41ba1f5565f9b9ee76264b3f97488b5
Ngombabu <chrisngombabu2@gmail.com>
2026-03-20 18:45:07 +0100

    couleur, camera, logo

--- superviseur/login.php
+++ superviseur/login.php
@@ -115,7 +115,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     echo json_encode([
         'success' => true,
         'message' => 'Connexion réussie',
-        'redirect' => '/POINTAGE/SCAN/scan.html'
+        'redirect' => '../SCAN/scan.html'
     ]);
     exit;
 }
